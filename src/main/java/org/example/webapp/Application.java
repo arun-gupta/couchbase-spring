4f03677e54479aeb78f94960d5a6c1ce59d83c78
Josh Long <josh@joshlong.com>
2017-01-25 00:14:06 -0800

    introduced Spring Boot's Actuator and a `CouchbaseHealthIndicator`

--- src/main/java/org/example/webapp/Application.java
+++ src/main/java/org/example/webapp/Application.java
@@ -1,29 +1,44 @@
 package org.example.webapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.couchbase.client.java.Bucket;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.CouchbaseHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.couchbase.core.CouchbaseOperations;
 
 /**
- * @arungupta
+ * @author arungupta
+ * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-    @Autowired
-    private BookRepository repository;
+	@Bean
+	CouchbaseHealthIndicator couchbaseHealthIndicator(CouchbaseOperations couchbaseOperations) {
+		return new CouchbaseHealthIndicator(couchbaseOperations);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Bean
+	HealthIndicator couchbaseBucketHealthIndicator(Bucket bucket) {
+		return () -> (!bucket.isClosed() ? Health.up() : Health.down()).build();
+	}
 
-    @Override
-    public void run(String... args) {
-        String isbn = "978-1-4919-1889-0";
-        repository.save(new Book(isbn, "Minecraft Modding with Forge", "29.99"));
+	@Bean
+	CommandLineRunner demo(BookRepository repository) {
+		return args -> {
+			String isbn = "978-1-4919-1889-0";
+			repository.save(new Book(isbn, "Minecraft Modding with Forge", "29.99"));
 
-        Book book = repository.findByIsbn(isbn);
-        System.out.println(book);
-    }
+			Book book = repository.findByIsbn(isbn);
+			System.out.println(book);
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
